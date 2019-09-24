81b2481e69cd88c160a25b890afa448ec413e470
henryo3o <18661139072@163.com>
2019-09-24 23:42:06 +0800

    init

--- App.vue
+++ App.vue
@@ -1,14 +1,7 @@
 <script>
 	export default {
 		onLaunch: function() {
-			uni.login({
-				success(res) {
-
-				}
-			})
-
 			console.log('App Launch')
-
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -19,20 +12,6 @@
 	}
 </script>
 
-<style lang="scss">
+<style>
 	/*每个页面公共css */
-	@import "/static/iconfont/font.scss";
-	
-	@import "./graceUI/graceIcons.css";
-	@import "./graceUI/graceUI.css";
-
-	page {
-		background-color: #fafafa;
-	}
-
-	view {
-		font-size: 28upx;
-		font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-		line-height: inherit;
-	}
 </style>
